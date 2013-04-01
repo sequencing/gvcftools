0e2369d563fda13a553a3e0bf06e2444f4525b09
Chris Saunders <csaunders@illumina.com>
2013-04-01 14:20:57 -0700

    add haeder to test code

--- src/libutil/test/test_main.cpp
+++ src/libutil/test/test_main.cpp
@@ -1,9 +1,29 @@
-#define BOOST_TEST_MODULE libutil
-//#define BOOST_TEST_MAIN
-//#define BOOST_TEST_ALTERNATIVE_INIT_API
-#include "boost/test/unit_test.hpp"
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Copyright (c) 2009-2012 Illumina, Inc.
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
 
-//BOOST_AUTO_TEST_CASE( foo ) {
-//                BOOST_CHECK_EQUAL(2, 2);
-//}
+#define BOOST_TEST_MODULE libutil
+#include "boost/test/unit_test.hpp"
 
