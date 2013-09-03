ebe23206b52831001fb203f82c2180805731878f
Chris Saunders <csaunders@illumina.com>
2013-09-03 11:42:10 -0700

    add a standard code formatting script

---
+++ scratch/run_astyle.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# if asytle is found, then run it on the code with lightweight re-formatting options
+#
+
+set -o nounset
+
+if ! which -a astyle > /dev/null 2>&1 ; then exit 0; fi
+
+thisDir=$(dirname $0)
+
+cd $thisDir/../gvcftools/src
+
+
+# conservative code re-formatting:
+astyle \
+--align-pointer=type \
+--keep-one-line-blocks \
+--keep-one-line-statements \
+--max-instatement-indent=80 \
+--min-conditional-indent=0 \
+--pad-header \
+--lineend=linux \
+--recursive \
+*.cpp *.hh *.h
+
